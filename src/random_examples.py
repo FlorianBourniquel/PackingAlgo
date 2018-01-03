631e1391547eb65a11e5fbcf56482c807f901669
florian <florian576@gmail.com>
2018-01-03 16:16:43 +0100

    Ajout du nombre d'objet present dans l'exemple à la fin du fichier
    fix: ajout du point à la fin du fichier

--- src/random_examples.py
+++ src/random_examples.py
@@ -3,16 +3,19 @@ import random
 
 def generate_random_example( dir, name):
     example_file = open(dir + name, 'w')
-    example_file.write("Taille bin\n")
-    example_file.write(str(random.randint(1, 1000)) + "\n")
-    example_file.write("Objets\n")
 
+    #we define a random size for a bin
+    output = "Taille bin\n"
+    output += str(random.randint(10, 1000)) + "\n"
+
+    #we create objects
+    output += "Objets\n"
     nb_object = random.randint(1, 10000)
-    random_size_objects = []
-    for i in range(0, nb_object):
-        example_file.write(str(random.randint(1, 1000)) + ", ")
+    for i in range(0, nb_object-1):
+        output += str(random.randint(1, 1000)) + ", "
+    output += str(random.randint(1, 1000)) + "."
 
-
+    example_file.write(output)
     example_file.close()
 
 generate_random_example("../example", "example")
